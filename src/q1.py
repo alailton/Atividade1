0e81528a1d16c9a4e5bb15a615f71b6d2854f55a
user <user@labcomp48682u>
2017-03-09 15:49:40 -0300

    first commit

---
+++ src/q1.py
@@ -0,0 +1,13 @@
+'''
+Created on 9 de mar de 2017
+
+@author: Alailton (alailton23@gmail.com)
+'''
+
+from math import log 
+
+number = int(input("Tamanho da mensagem :"));
+
+bits = int( log( number, 2) + 1);
+
+print("Qtd de bits = {0}".format(bits));
